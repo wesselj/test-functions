ed912909329c73a064f691812a5a365abaa21763
Jørgen Wessel <joergen.wessel@cognitedata.com>
2022-12-01 19:02:55 +0000

    Modified handler.py

--- execute_rest_extractor/handler.py
+++ execute_rest_extractor/handler.py
@@ -1,15 +1,9 @@
-from common.utilities import sum_integers
+import os
+from rest_extractor import extractor
 
-
-def handle(data, client):
-    print("I got the following data:")
-    print(data)
-
-    if not ("a" in data and "b" in data):
-        raise KeyError("Data should contain both keys: 'a' and 'b'")
-
-    data["sum"] = sum_integers(data["a"], data["b"])
-
-    print("Will now return updated data")
-
-    return data
+def handle(secrets, data):
+   print("running rest extractor")
+   os.environ["COGNITE_CLIENT_SECRET"] = secrets.get("client-secret")
+   os.environ["BACKFILL_MIN"] = data.get("backfill_min", "")
+   extractor.main()
+   print("running rest extractor done")
