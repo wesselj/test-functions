a09c27c01379e554c0df347891b3075d16b154b0
Joergen Wessel <joergen.wessel@cognite.com>
2022-12-03 11:46:09 +0100

    Updated functions correcting import

--- execute_csv_extractor_assets/handler.py
+++ execute_csv_extractor_assets/handler.py
@@ -1,9 +1,8 @@
 import os
-from execute_csv_extractor_assets import extractor
+from csv_extractor_simple import extractor
 
 def handle(secrets, data):
    print("running rest extractor")
    os.environ["COGNITE_CLIENT_SECRET"] = secrets.get("client-secret")
-   os.environ["BACKFILL_MIN"] = data.get("backfill_min", "")
    extractor.main()
    print("running rest extractor done")
--- execute_csv_extractor_timeseries/handler.py
+++ execute_csv_extractor_timeseries/handler.py
@@ -4,6 +4,5 @@ from csv_extractor_simple import extractor
 def handle(secrets, data):
    print("running rest extractor")
    os.environ["COGNITE_CLIENT_SECRET"] = secrets.get("client-secret")
-   os.environ["BACKFILL_MIN"] = data.get("backfill_min", "")
    extractor.main()
    print("running rest extractor done")
