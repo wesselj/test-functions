6907cf2453749fb1b09cc9a64bffa8eaea6928c1
Joergen Wessel <joergen.wessel@cognite.com>
2022-12-03 11:54:24 +0100

    Removed client-secret fro some functions to test

--- execute_csv_extractor_timeseries/handler.py
+++ execute_csv_extractor_timeseries/handler.py
@@ -3,6 +3,6 @@ from csv_extractor_simple import extractor
 
 def handle(secrets, data):
    print("running rest extractor")
-   os.environ["COGNITE_CLIENT_SECRET"] = secrets.get("client-secret")
+   #os.environ["COGNITE_CLIENT_SECRET"] = secrets.get("client-secret")
    extractor.main()
    print("running rest extractor done")
